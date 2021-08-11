e1eebe884b47da80025022e8ccaaedf9d24f7400
shafs <shafichowdhury003@gmail.com>
2021-08-11 08:13:20 +0600

    wk

---
+++ class 22/js/script.js
@@ -0,0 +1,34 @@
+$(function(){
+    'use strict';
+
+    //slider switch 
+
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
