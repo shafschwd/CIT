ce1caed62f149eceab641847081d7e75122ce930
shafs <shafichowdhury003@gmail.com>
2021-09-12 02:01:44 +0600

    27

---
+++ class 27/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+	"use strict";
+
+	//Banner slider
+	$("#banner").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		dots: true,
+	});
+});
