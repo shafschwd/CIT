96719a51bd55150727f9888554a576b599c34f0c
shafs <shafichowdhury003@gmail.com>
2021-09-15 13:25:03 +0600

    28

---
+++ class 28/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+	"use strict";
+
+	//Banner slider
+	$("#banner").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		dots: true,
+	});
+
+	//venobox
+	$(".venobox").venobox();
+});
